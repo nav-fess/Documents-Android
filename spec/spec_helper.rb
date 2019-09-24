a05e6bd992da4e0cd75dcf954304b779ab7389b1
Egor Rozhentsov <egor.rozhentsov@gmail.com>
2019-09-24 18:44:51 +0300

    Fixed paths for spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,12 @@
 require 'appium_lib'
 
 RSpec.configure do |config|
-  appium_config = File.join(File.dirname(__FILE__), 'config', 'appium.txt')
+  appium_config = File.join(File.dirname(__FILE__), '..', 'config', 'appium.txt')
   capabilities = Appium.load_settings file: appium_config
-  capabilities[:caps][:app] = File.join(File.dirname(__FILE__), 'spec', 'Documents.apk')
+  capabilities[:caps][:app] = File.join(File.dirname(__FILE__), 'Documents.apk')
 
   config.before(:all) do
-    Appium::Driver.new(appium_capabilities, true)
+    Appium::Driver.new(capabilities, true)
     Appium.promote_appium_methods Object
 
     @driver.start_driver
