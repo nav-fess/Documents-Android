d08152b8e7c46acf6fdf2335416385395c8f467f
Egor Rozhentsov <egor.rozhentsov@gmail.com>
2019-09-24 18:36:51 +0300

    Merged env.rb and spec_helper

--- env.rb
+++
@@ -1,9 +0,0 @@
-require 'appium_lib'
-
-def appium_capabilities
-  appium_config = File.join(File.dirname(__FILE__), 'config', 'appium.txt')
-  capabilities = Appium.load_settings file: appium_config
-  capabilities[:caps][:app] = File.join(File.dirname(__FILE__), 'spec', 'Documents.apk')
-  capabilities.to_hash
-end
-
