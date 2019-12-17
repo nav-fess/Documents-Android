b371c1580535715173e8c52038413e86131aafd8
Egor Rozhentsov <egor.rozhentsov@gmail.com>
2019-12-17 18:19:52 +0300

    Supplemented spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,23 +2,37 @@
 
 require 'appium_lib'
 require 'json'
-require_relative '../framework/constants/id.rb'
-require_relative '../framework/constants/const_index.rb'
-require_relative '../framework/constants/xpath.rb'
-require_relative '../framework/tools/appium_extension.rb'
+
+# Constants
+require_relative '../framework/constants/id'
+require_relative '../framework/constants/const_index'
+require_relative '../framework/constants/xpath'
+
+# Helpers
 require_relative '../framework/helpers/add_storage_helper.rb'
 require_relative '../framework/helpers/login_helpers.rb'
 require_relative '../framework/helpers/registration_helper.rb'
 require_relative '../framework/helpers/helpers.rb'
+require_relative '../framework/helpers/plus_fab'
+require_relative '../framework/helpers/open_section'
+require_relative '../framework/helpers/share'
+
+# Tools
+require_relative '../framework/tools/appium_extension'
+require_relative '../framework/tools/auth_data_tools'
+
 include AppiumExtension
 include Helpers
 
 RSpec.configure do |config|
   config.filter_run_including foo: :bar
   config.silence_filter_announcements = true
+
   appium_config = File.join File.dirname(__FILE__), '..', 'config', 'appium.txt'
   capabilities = Appium.load_settings file: appium_config
   capabilities[:caps][:app] = File.join(File.dirname(__FILE__), '..', 'apk', 'Documents.apk')
+
+  Selenium::WebDriver.logger.level = :error
 
   config.before(:all) do
     Appium::Driver.new(capabilities, true)
