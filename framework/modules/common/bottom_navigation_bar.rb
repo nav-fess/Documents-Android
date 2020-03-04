8b86418ab8ffd393462883f724c008cdb0e24142
Egor Rozhentsov <egor.rozhentsov@gmail.com>
2020-03-04 16:19:56 +0300

    Added method for getting active section

--- framework/modules/common/bottom_navigation_bar.rb
+++ framework/modules/common/bottom_navigation_bar.rb
@@ -2,8 +2,14 @@
 
 # PageObject for BottomNavigationBar
 class BottomNavigationBar < BasePageObject
+  layout 'main', id: 'bottom_navigation'
+
   button 'recent', id: 'menu_item_recent'
   button 'clouds', id: 'menu_item_cloud'
   button 'on_device', id: 'menu_item_on_device'
   button 'profile', id: 'menu_item_setting'
+
+  def self.active_section
+    find_nested_element parent: main_layout, xpath: '//android.widget.TextView[@selected="true"]'
+  end
 end
