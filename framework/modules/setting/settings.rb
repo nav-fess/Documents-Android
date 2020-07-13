8cbce974bd60df4f718868e20e537167739011af
Vladimir Voronin <nav-fess@yandex.ru>
2020-07-13 18:29:11 +0300

    add spec: help adn feedback

--- framework/modules/setting/settings.rb
+++ framework/modules/setting/settings.rb
@@ -5,6 +5,8 @@ class Settings < BasePageObject
   CLEAR_SIZE_CACHE = '0.00 Bytes'
   PATH_TO_FILE = 'sdcard/OnlyOffice/'
   NAME_FILE = 'big_size.pptx'
+  HELP_URL = 'https://helpcenter.onlyoffice.com/mobile-applications/documents/android/'
+  CHOOSE_EMAIL = 'Choose email client...'
 
   button 'switch', id: 'wifiSwitch'
   layout 'clear_cache', id: 'clearCacheLayout'
@@ -12,4 +14,8 @@ class Settings < BasePageObject
   button 'about', id: 'settingAboutItem'
   button 'help', id: 'settingHelpItem'
   button 'feedback', id: 'settingSupportItem'
+
+  text 'help_url', xpath: '//android.widget.EditText'
+  textfield 'edit_feedback', id: 'dialogCommonEditMultilineValueEdit'
+  text 'tab_emails', xpath: '//android.widget.TextView'
 end
---
+++ spec/manager/settings/help_and_feedback_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'Help and Feedbac', :settings_1 do
+  before :all do
+    Onboarding.skip_button_click
+  end
+
+  it 'Help : Tap button help' do
+    BottomNavigationBar.profile_button_click delay: 3
+    Account.settings_button_click delay: 3
+    Settings.help_button_click
+  end
+
+  it 'Help : Check text link' do
+    help_url = Settings.help_url_texts[0].text
+    expect(help_url).to eq Settings::HELP_URL
+    back
+  end
+
+  it 'Feedbac : Fill textfield' do
+    Settings.feedback_button_click
+    Settings.edit_feedback_textfield_fill 'NCT auto mobile test. SKIP PLEASE'
+    Dialog.accept_button_click
+  end
+
+  it 'Feedbac : Check a mail client' do
+    choose_email = Settings.tab_emails_text_value delay: 5
+    expect(choose_email).to eq Settings::CHOOSE_EMAIL
+  end
+end
